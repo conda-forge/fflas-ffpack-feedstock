6d26a076bf848daeb06885a08353afa50cea907b
Isuru Fernando <isuruf@gmail.com>
2025-10-28 10:43:14 -0700

    use dynamic blas

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,16 +14,6 @@ elif [[ "$cxx_compiler" == "gxx" ]]; then
     export CCNAM=gcc
 fi
 
-# Statically link in openblas since fflas-ffpack require a single threaded blas.
-# If linked dynamically, it will make openblas single threaded for the entire
-# process since fflas uses openblas_set_num_threads(1)
-if [[ "$target_platform" == "linux-"* ]]; then
-    BLAS_LIBS="-Wl,${PREFIX}/lib/libopenblas.a -Wl,--exclude-libs,libopenblas.a -lgfortran -lpthread"
-elif [[ "$target_platform" == "osx-"* ]]; then
-    BLAS_LIBS="-L${PREFIX}/lib -Wl,-hidden-lopenblas -lgfortran -lpthread"
-    rm ${PREFIX}/lib/libopenblas.dylib
-fi
-
 chmod +x configure
 # Enable only SSE/SSE2 as these are supported on all 64bit CPUs
 # https://unix.stackexchange.com/a/249384
@@ -31,18 +21,15 @@ chmod +x configure
     --prefix="$PREFIX" \
     --libdir="$PREFIX/lib" \
     --with-default="$PREFIX" \
-    --with-blas-libs="${BLAS_LIBS}" \
+    --with-blas-libs="-llapack -lcblas -lblas" \
     --enable-precompilation \
     --disable-openmp \
     --without-archnative || (cat config.log; false)
 
-make -j${CPU_COUNT} LIBS="${BLAS_LIBS}" BLAS_LIBS="${BLAS_LIBS}"
-make install LIBS="${BLAS_LIBS}" BLAS_LIBS="${BLAS_LIBS}"
+make -j${CPU_COUNT}
+make install
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
     # Provide information requested by https://github.com/linbox-team/fflas-ffpack/issues/408#issuecomment-2770670149
-    make check -j${CPU_COUNT} LIBS="${BLAS_LIBS}" BLAS_LIBS="${BLAS_LIBS}" || (cat tests/test-suite.log && ldd tests/test-echelon && exit 1)
+    make check -j${CPU_COUNT} || (cat tests/test-suite.log && ldd tests/test-echelon && exit 1)
 fi
-
-sed -i "s@${BLAS_LIBS}@@g" ${PREFIX}/bin/fflas-ffpack-config
-sed -i "s@${BLAS_LIBS}@@g" ${PREFIX}/lib/pkgconfig/fflas-ffpack.pc
